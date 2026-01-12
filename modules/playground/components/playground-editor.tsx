dfd629f2f193817323a83801166e47169ab5f24f
Aryan <aryanpatil1406@gmail.com>
2026-01-12 15:28:26 +0530

    implemented monaco editor

---
+++ modules/playground/components/playground-editor.tsx
@@ -0,0 +1,15 @@
+"use client" 
+
+import {useRef, useCallback , useEffect} from 'react'
+import React from 'react'
+
+const Playground_Editor = () => {
+  return (
+    <div >
+
+      
+    </div>
+  )
+}
+
+export default Playground_Editor
